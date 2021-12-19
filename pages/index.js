6f4ac5e7cc389ce89126283bf83e3e4663ee82bd
Madhu <madhu.m@nokia.com>
2021-12-19 17:42:22 +0530

    ++ configured notus js css template

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,12 @@ export default function Home() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+
+      <br />
+
+      <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-600 bg-blueGray-200 uppercase last:mr-0 mr-1">
+        blueGray
+      </span>
     </div>
   )
 }
